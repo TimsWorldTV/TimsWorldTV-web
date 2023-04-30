7a372667b5e3d2970f542a0504903b56ac5a5137
TimsWorldTV <83842453+TimsWorldTV@users.noreply.github.com>
2023-04-30 17:02:53 +0200

    stldown

--- 3d/files.php
+++ 3d/files.php
@@ -1,10 +1,19 @@
 <?php
+    // Bestimme den Pfad zum Ordner
     $dir = './stl';
+
+    // Liste alle Dateien und Ordner im Ordner auf
     $files = scandir($dir);
+
+    // Schleife über alle Dateien und Ordner
     foreach ($files as $file) {
-        if ($file != '.' && $file != '..') {
+        // Überprüfe, ob es sich um einen Ordner handelt
+        if (is_dir($dir.'/'.$file) && $file != '.' && $file != '..') {
+            // Wenn es sich um einen Ordner handelt, gib einen Link aus, um ihn zu betreten
+            echo "<div class='file'><a href='stl/$file/'>$file/</a></div>";
+        } elseif ($file != '.' && $file != '..') {
+            // Wenn es sich um eine Datei handelt, gib einen Download-Link aus
             echo "<div class='file'><a href='$dir/$file' download>$file</a></div>";
         }
     }
-    
 ?>
--- 3d/stldown.php
+++ 3d/stldown.php
@@ -31,9 +31,29 @@
         </ul> 
       </div>
       <div class="content">
-        <h1>Tims 3D World(T3DW)</h1>
-        <p> Alles rund um FreeCAD</p>
-        <?php include('files.php'); ?>
+        <h1>STL Downloads</h1>
+        <p> Hier gibt es alle aktuellen STL datein</p>
+
+        <?php
+            // Bestimme den Pfad zum Ordner
+            $dir = './stl';
+
+            // Liste alle Dateien und Ordner im Ordner auf
+            $files = scandir($dir);
+
+            // Schleife über alle Dateien und Ordner
+            foreach ($files as $file) {
+                // Überprüfe, ob es sich um einen Ordner handelt
+                if (is_dir($dir.'/'.$file) && $file != '.' && $file != '..') {
+                    // Wenn es sich um einen Ordner handelt, gib einen Link aus, um ihn zu betreten
+                    echo "<div class='file'><a href='stl/$file/'>$file/</a></div>";
+                } elseif ($file != '.' && $file != '..') {
+                    // Wenn es sich um eine Datei handelt, gib einen Download-Link aus
+                    echo "<div class='file'><a href='$dir/$file' download>$file</a></div>";
+                }
+            }
+        ?>
+
       </div>
       
     </div>
